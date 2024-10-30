a4cd2e815f1fd4ceb5219edc2196c81606657ced
Leandro Emanuel López <lopezlean@gmail.com>
2024-10-30 11:33:56 +0100

    Fix: remove monaco react because it conflict with wordpress plugin guidelines

--- src/dashboard/app/src/components/form/html-editor.tsx
+++ src/dashboard/app/src/components/form/html-editor.tsx
@@ -1,25 +1,4 @@
-import React from 'react';
-
-import { Editor, EditorProps } from '@monaco-editor/react';
-import { InputBaseComponentProps } from '@mui/material/InputBase';
-
 import TextField, { TextFieldProps } from './text-field';
-
-const EditorInternal = React.forwardRef((props: InputBaseComponentProps) => {
-  return (
-    <Editor
-      defaultLanguage="html"
-      width={'100%'}
-      height={'20vh'}
-      {...(props as EditorProps)}
-      options={{
-        minimap: {
-          enabled: false,
-        },
-      }}
-    />
-  );
-});
 
 export default function HtmlEditor(props: TextFieldProps) {
   return (
@@ -27,9 +6,6 @@ export default function HtmlEditor(props: TextFieldProps) {
       {...props}
       multiline={true}
       rows={10}
-      InputProps={{
-        inputComponent: EditorInternal,
-      }}
       InputLabelProps={{
         shrink: true,
       }}
